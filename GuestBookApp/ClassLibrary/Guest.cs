4cf48ef205b6c0eaf28347cc084c3468d081c6f9
CKn00b <cknew.dev@gmail.com>
2020-11-06 16:23:05 -0500

    Add files via upload

---
+++ GuestBookApp/ClassLibrary/Guest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary
+{
+    public class Guest
+    {
+        public string name { get; set; }
+        public string message { get; set; }
+        public DateTime messageTime { get; set; }
+
+        public static Guest NewEntry()
+        {
+            Console.WriteLine("Enter your name: ");
+            string newName = Console.ReadLine();
+            Console.WriteLine("Enter your message: ");
+            string newMessage = Console.ReadLine();
+            Guest newGuest = new Guest()
+            {
+                name = newName,
+                message = newMessage,
+                messageTime = DateTime.Now
+            };
+
+            return newGuest;
+        }
+
+        public static List<Guest> AddToGuestList(List<Guest> GuestList, Guest newGuest)
+        {
+            GuestList.Add(newGuest);
+            Console.WriteLine(GuestList.Count);
+            return GuestList;
+        }
+    }
+}
---
+++ GuestBookApp/ClassLibrary/Utils.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace ClassLibrary
+{
+    public class Utils
+    {
+        public static void DisplayTitle()
+        {
+            Console.WriteLine("Guest Book");
+        }
+        public static void MainMenu()
+        {
+            Console.WriteLine("\n1. Add Entry \n2. Exit");
+        }
+
+        public static string GetChoice()
+        {
+            Console.WriteLine("Please enter your option: ");
+            string choice = Console.ReadLine();
+            return choice;
+        }
+
+        
+    }
+}
